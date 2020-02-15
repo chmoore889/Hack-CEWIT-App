dbf7d10584141b385b956be9748cdded36904713
chmoore889 <chmoore889@gmail.com>
2020-02-14 22:23:47 -0500

    Added timer input

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_picker/flutter_picker.dart';
+
 import 'profile.dart';
 import 'dashboard.dart';
 import 'character.dart';
 import 'stats.dart';
+import 'dart:convert';
+import 'pickerData.dart';
 
 class Home extends StatefulWidget  {
   @override
@@ -10,6 +14,8 @@ class Home extends StatefulWidget  {
 }
 
 class HomePage extends State<Home> {
+
+  int minutes = 0;
 
   int currentTab = 0;
   Widget currentScreen = Dashboard();
@@ -20,7 +26,7 @@ class HomePage extends State<Home> {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage("assets/images/background.jpg"),
+            image: AssetImage("assets/images/background.png"),
             fit: BoxFit.cover,
           ),
         ),
@@ -28,7 +34,7 @@ class HomePage extends State<Home> {
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {showPickerDateRange(context);},
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomAppBar(
@@ -137,6 +143,55 @@ class HomePage extends State<Home> {
           ),
         ),
       ),
+    );
+  }
+
+  showPickerDateRange(BuildContext context) {
+    Picker timeSel = new Picker(
+      hideHeader: true,
+      adapter: PickerDataAdapter<String>(
+          pickerdata: JsonDecoder().convert(PickerData),
+          isArray: true,
+      ),
+      onConfirm: (Picker picker, List value) {
+        print(value.toString());
+        print(picker.getSelectedValues());
+      }
+    );
+
+    List<Widget> actions = [
+      FlatButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: new Text(PickerLocalizations.of(context).cancelText)),
+      FlatButton(
+        onPressed: () {
+          Navigator.pop(context);
+          timeSel.onConfirm(timeSel, timeSel.selecteds);
+        },
+        child: new Text(PickerLocalizations.of(context).confirmText)
+      )
+    ];
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return new AlertDialog(
+          title: Text("Set Timer"),
+          actions: actions,
+          content: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text("Hours:                 Minutes:"),
+                timeSel.makePicker(),
+              ],
+            ),
+          ),
+        );
+      }
     );
   }
 }
